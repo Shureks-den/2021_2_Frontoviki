30ac1b4f8c6bafd1ce571bda27a48d28cffb2751
aleksandr.shibanov <aleksandr.shibanov@corp.mail.ru>
2021-12-16 00:07:26 +0300

    added chat tests

--- Makefile
+++ Makefile
@@ -11,6 +11,6 @@ lint:
 ## test-func: run go test func
 test:
 	cd ${TEST_DIR}
-	go test -coverpkg=./... -coverprofile=cover ./... && cat cover | grep -v "mock" | grep -v "database" | grep -v "cmd" | grep -v  "easyjson" | grep -v "proto" | grep -v "config" | grep -v "metrics" | grep -v "error" | grep -v "mocks" > cover.out && go tool cover -func=cover.out
-	rm -rf cover
-	rm -rf cover.out
+	go test -coverpkg=./... -coverprofile=cover ./... && cat cover | grep -v "logs" | grep -v "mock" | grep -v "database" | grep -v "cmd" | grep -v  "easyjson" | grep -v "proto" | grep -v "config" | grep -v "metrics" | grep -v "error" | grep -v "mocks" > cover.out && go tool cover -func=cover.out
+	rm -f cover
+	rm -f cover.out
--- internal/services/chat/server/server_test.go
+++ internal/services/chat/server/server_test.go
@@ -1,12 +1,99 @@
 package delivery
 
-// func TestCartSuccess(t *testing.T) {
-// 	cu := mocks.ChatUsecase{}
-// 	su := NewChatGRPCServer(logrus.New(), &cu)
+import (
+	"context"
+	"testing"
+	"time"
 
-// 	cu.On("GetHistory", mock.Anything).Return(nil, nil)
+	mocks "yula/internal/services/chat/mocks"
+	proto "yula/proto/generated/chat"
 
-// 	_, err := su.GetHistory(nil, &proto.GetHistoryArg{})
-// 	assert.Nil(t, err)
-// 	assert.Equal(t, nil, nil)
-// }
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func TestGetHistorySuccess(t *testing.T) {
+	cu := mocks.ChatUsecase{}
+	su := NewChatGRPCServer(logrus.New(), &cu)
+
+	cu.On("GetHistory", mock.Anything, int64(1), int64(10)).Return(nil, nil)
+
+	_, err := su.GetHistory(context.Background(), &proto.GetHistoryArg{
+		DI: &proto.DialogIdentifier{
+			Id1:   1,
+			Id2:   2,
+			IdAdv: 3,
+		},
+		FP: &proto.FilterParams{
+			Offset: 1,
+			Limit:  10,
+		},
+	})
+	assert.Nil(t, err)
+}
+
+func TestCreateSuccess(t *testing.T) {
+	cu := mocks.ChatUsecase{}
+	su := NewChatGRPCServer(logrus.New(), &cu)
+
+	cu.On("Create", mock.AnythingOfType("*models.Message")).Return(nil)
+
+	_, err := su.Create(context.Background(), &proto.Message{
+		MI: &proto.MessageIdentifier{
+			IdFrom: 1,
+			IdTo:   2,
+			IdAdv:  3,
+		},
+		Msg:       "aboba",
+		CreatedAt: timestamppb.New(time.Now()),
+	})
+	assert.Nil(t, err)
+}
+
+func TestCreateDialogSuccess(t *testing.T) {
+	cu := mocks.ChatUsecase{}
+	su := NewChatGRPCServer(logrus.New(), &cu)
+
+	cu.On("CreateDialog", mock.AnythingOfType("*models.Dialog")).Return(nil)
+
+	_, err := su.CreateDialog(context.Background(), &proto.Dialog{
+		DI: &proto.DialogIdentifier{
+			Id1:   1,
+			Id2:   2,
+			IdAdv: 3,
+		},
+		CreatedAt: timestamppb.New(time.Now()),
+	})
+
+	assert.Nil(t, err)
+}
+
+func TestClearSuccess(t *testing.T) {
+	cu := mocks.ChatUsecase{}
+	su := NewChatGRPCServer(logrus.New(), &cu)
+
+	cu.On("Clear", mock.AnythingOfType("*models.IDialog")).Return(nil)
+
+	_, err := su.Clear(context.Background(), &proto.DialogIdentifier{
+		Id1:   1,
+		Id2:   2,
+		IdAdv: 3,
+	})
+
+	assert.Nil(t, err)
+}
+
+func TestGetDialogsSuccess(t *testing.T) {
+	cu := mocks.ChatUsecase{}
+	su := NewChatGRPCServer(logrus.New(), &cu)
+
+	cu.On("GetDialogs", int64(10)).Return(nil, nil)
+
+	_, err := su.GetDialogs(context.Background(), &proto.UserIdentifier{
+		IdFrom: 10,
+	})
+
+	assert.Nil(t, err)
+}
